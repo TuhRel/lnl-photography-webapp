ff94f92f3b930adc2662b3e9b5b0291b861bae6f
Tony Johnson <tonytj0106@gmail.com>
2025-10-14 14:35:48 -0400

    variables for deployment

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,16 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Replace with your Firebase config
-// Get this from Firebase Console > Project Settings > General > Your apps
 const firebaseConfig = {
-  apiKey: "AIzaSyCAGZ03maXJ8h5x0_IXA9vwLMzYRXoEnJM",
-  authDomain: "lnl-photography.firebaseapp.com",
-  projectId: "lnl-photography",
-  storageBucket: "lnl-photography.firebasestorage.app",
-  messagingSenderId: "695825471257",
-  appId: "1:695825471257:web:1222a2332fdd0f5a4d1788",
-  measurementId: "G-21GR9RTPCX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
